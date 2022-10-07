ca8c47371ab37ccf401dad6a3bceacae37948fcb
Pokesi <81540683+Pokesi@users.noreply.github.com>
2022-10-07 22:42:15 +1100

    add mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { ThemeProvider } from 'styled-components';
 import { ThorinGlobalStyles, lightTheme, Card, Profile, Heading, FieldSet, Input as TInput, Toast, Button } from '@ensdomains/thorin';
+import { isMobile } from 'react-device-detect';
 import { ethers, Contract, providers } from 'ethers';
 import toast, { Toaster } from 'react-hot-toast';
 
@@ -33,13 +34,89 @@ function App() {
 
   if (website.length > 0) {
     return (
-    <>
-      <Toaster/>
-      <div id="content">
-        <iframe width="100%" height="100%" frameBorder="0" src={website}></iframe>
+      <div style={{
+        maxHeight: '100%'
+      }}>
+        <ThemeProvider theme={lightTheme}>
+          <Toaster/>
+          <ThorinGlobalStyles />
+          <div id="content">
+            <Button style={{
+              borderRadius: '0px',
+            }} shadowless onClick={() => {
+              setWebsite('');
+            }}>Close website</Button>
+            <iframe width="100%" height="100%" frameBorder="0" src={website}></iframe>
+          </div>
+        </ThemeProvider>
       </div>
-    </>
     )
+  }
+
+  if (isMobile) {
+    return (
+      <ThemeProvider theme={lightTheme}>
+        <ThorinGlobalStyles />
+        <Card className="box-m" shadow>
+          <img src="https://rave.domains/RaveBase.png" style={{
+            cursor: 'pointer',
+            paddingBottom: '7.5px'
+          }}/>
+          <Heading align="center" style={{
+            fontSize: "5.5vh"
+          }}>{(window.location.host.split('.').length === 3) ? name : 'Not a Valid Name'}</Heading>
+          <Card style={{
+            backgroundColor: '#0fdca1',
+            marginTop: '20px',
+            width: '100%',
+          }}><p style={{
+            textAlign: 'center',
+            fontSize: '3vh',
+            color: '#FFF'
+          }}><a href={`https://ftmscan.com/address/${address}`}>{truncateAddress(address)}</a> | {name}</p></Card>
+        </Card>
+        <Card className="box-m" shadow>
+          <FieldSet legend="Set Your Website!">
+            <TInput
+              label="Website"
+              placeholder={`${name}.rip`}
+              prefix="https://"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setInput(e.target.value);
+              }}
+              onKeyDown={(e: any) => {
+                console.log(e.keyCode);
+                if (e.keyCode === 13) {
+                  toast.loading('Saving...', { duration: 6000 });
+                  // @ts-ignore
+                  window.ethereum.request({
+                    method: "wallet_addEthereumChain",
+                    params: [{
+                      chainId: "0xFA",
+                      rpcUrls: ["https://rpc.ftm.tools", "https://rpc-api.fantom.network"],
+                      chainName: "Fantom Opera",
+                      nativeCurrency: {
+                          name: "FTM",
+                          symbol: "FTM",
+                          decimals: 18
+                      },
+                      blockExplorerUrls: ["https://ftmscan.com/"]
+                    }]
+                  });
+                  // @ts-ignore
+                  const provider = new ethers.providers.Web3Provider(window.ethereum, 250);
+                  let signer = provider.getSigner();
+                  new Contract('0x2f680945b96329ae0109dde11adb2d81467379db', externalAbi, signer).setText(name, 'website', `https://${input}`).catch((e: any) => {
+                    toast.error(e.message, { style: { minWidth: '100%', wordWrap: 'break-word' } })
+                  });
+                }
+              }}
+            />
+          </FieldSet>
+        </Card>
+        <Toaster/>
+      </ThemeProvider>
+    );
   }
 
   return (
